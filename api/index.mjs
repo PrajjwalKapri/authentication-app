8223375e45956c8a23fb7aab75e6ec9840792adf
prajjwalkapri <prajjwalkapri@gmail.com>
2024-03-28 19:20:51 +0530

    Connected to MongoDb

--- api/index.mjs
+++ api/index.mjs
@@ -1,8 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
   console.log("Server Started at port ", PORT);
 });
