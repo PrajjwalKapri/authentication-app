3edf20caa6ecc65464dc2233e97457b1da47da88
prajjwalkapri <prajjwalkapri@gmail.com>
2024-03-28 18:09:47 +0530

    Created  vite and tailwind css template and set up routes for the pages

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
+}
+
+export default App;
